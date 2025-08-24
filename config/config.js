b20c047e63572c8cac53de75a3c8ced59b05f87a
predator522 <p47017988@gmail.com>
2025-08-24 12:33:54 +0000

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,21 @@
+{
+  "botName": "T3RROR v1",
+  "version": "1.0.0",
+  "prefix": ".",
+  "ownerAlias": "DarkLord",
+  "owners": ["+233000000000"],
+  "features": {
+    "antilink": false,
+    "antibug": false,
+    "antispam": false,
+    "raidmode": false,
+    "welcome": true,
+    "goodbye": true,
+    "autoreply": false
+  },
+  "limits": {
+    "warningsBeforeKick": 3,
+    "spamWindowMs": 6000,
+    "spamMaxMsgs": 8
+  }
+}
