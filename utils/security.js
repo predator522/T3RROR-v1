00914aa3ec55bffb630dd87e109a65ff44a5568b
predator522 <p47017988@gmail.com>
2025-08-25 08:46:55 +0000

    Update security.js

--- utils/security.js
+++ utils/security.js
@@ -1,4 +1,6 @@
-import { config } from "../config/index.js";
+import { config } from "./config/index.js";
+
+console.log(config.botName); // example usage
 import msgs from "./config/messages.json";  
 
 const spamCache = new Map();
