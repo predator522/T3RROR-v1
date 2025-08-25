57d473d342991ea198b96e774ee41cda31d6ccbb
predator522 <p47017988@gmail.com>
2025-08-25 08:18:40 +0000

    Create index.js

---
+++ config/index.js
@@ -0,0 +1,5 @@
+import configData from "./config.json" assert { type: "json" };
+import messagesData from "./messages.json" assert { type: "json" };
+
+export const config = configData;
+export const msgs = messagesData;
