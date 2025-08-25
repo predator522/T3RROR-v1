7f049e9a7fc4430820d1992eb08276334f9e5534
predator522 <p47017988@gmail.com>
2025-08-25 08:45:25 +0000

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,12 @@
-import configData from "./config.json" assert { type: "json" };
-import messagesData from "./messages.json" assert { type: "json" };
+
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const configPath = path.join(__dirname, "config.json");
+const configData = JSON.parse(fs.readFileSync(configPath, "utf-8"));
 
 export const config = configData;
-export const msgs = messagesData;
